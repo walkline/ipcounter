a5f992562269f6987c50fc3bff757740614ef6db
walkline <walkline.ua@gmail.com>
2020-07-23 23:44:56 +0300

    IPIndex. Completes implementations of ipv4 index. Adds benchmarks for Add function.

--- ipv4bucketindex.go
+++ ipv4bucketindex.go
@@ -1,16 +1,80 @@
 package ipcounter
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+var (
+	ErrBadIPAddress = errors.New("bad ip address")
+)
+
+const (
+	bucketSize = 0x100 // all possible combinations for byte
+)
+
+type zeroBytesType struct{}
+
+// IPv4BucketIndex implements index for v4 ips, uses buckets of empty maps
 type IPv4BucketIndex struct {
+	// buckets used to reduce number of thread locks.
+	//
+	// Key for bucket consist from last byte of ip address.
+	// Used last byte to achieve more equal load to buckets.
+	//
+	// Key sample:
+	// 		'176.32.103.200' => 200 => 0xC8
+	buckets     [bucketSize]map[string]zeroBytesType
+	bucketsLock [bucketSize]sync.RWMutex
 }
 
 func NewIPv4BucketIndex() *IPv4BucketIndex {
-	return &IPv4BucketIndex{}
+	index := &IPv4BucketIndex{}
+	for i := 0; i < bucketSize; i++ {
+		index.buckets[i] = map[string]zeroBytesType{}
+	}
+	return index
 }
 
-func (*IPv4BucketIndex) Add(ip string) error {
+func (i *IPv4BucketIndex) Add(ip string) error {
+	k, err := i.bucketKeyByIP(ip)
+	if err != nil {
+		return err
+	}
+
+	i.bucketsLock[k].Lock()
+
+	i.buckets[k][ip] = zeroBytesType{}
+
+	// better to not use defer here
+	// because it would take additional 20ns
+	i.bucketsLock[k].Unlock()
+
 	return nil
 }
 
-func (*IPv4BucketIndex) Len() (int64, error) {
-	return 0, nil
+func (i *IPv4BucketIndex) Len() (int64, error) {
+	counter := int64(0)
+	for itr := range i.buckets {
+		i.bucketsLock[itr].RLock()
+		counter += int64(len(i.buckets[itr]))
+		i.bucketsLock[itr].RUnlock()
+	}
+	return counter, nil
+}
+
+func (*IPv4BucketIndex) bucketKeyByIP(ip string) (int, error) {
+	i := strings.LastIndexByte(ip, '.')
+	if i < 0 {
+		return i, ErrBadIPAddress
+	}
+
+	key, err := strconv.Atoi(ip[i+1:])
+	if err != nil {
+		return -1, ErrBadIPAddress
+	}
+
+	return key, nil
 }
--- ipv4bucketindex_test.go
+++ ipv4bucketindex_test.go
@@ -1,7 +1,10 @@
 package ipcounter_test
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 
 	. "github.com/walkline/ipcounter"
 )
@@ -11,6 +14,14 @@ func TestLenForIPv4BucketIndex(t *testing.T) {
 		ips         []string
 		expectedLen int64
 	}{
+		{
+			[]string{},
+			0,
+		},
+		{
+			[]string{"1.1.1.1"},
+			1,
+		},
 		{
 			[]string{"1.1.1.1", "2.2.2.42", "3.3.3.205"},
 			3,
@@ -36,4 +47,74 @@ func TestLenForIPv4BucketIndex(t *testing.T) {
 			t.Errorf("expected result - %d, actual result - %d", c.expectedLen, l)
 		}
 	}
+}
+
+func TestAddForIPv4BucketIndex(t *testing.T) {
+	for _, c := range []struct {
+		ip          string
+		expectedErr error
+	}{
+		{
+			"1.1.1.1",
+			nil,
+		},
+		{
+			"240.240.240.240",
+			nil,
+		},
+		{
+			"",
+			ErrBadIPAddress,
+		},
+		{
+			"0.0.0.X",
+			ErrBadIPAddress,
+		},
+	} {
+		index := NewIPv4BucketIndex()
+
+		err := index.Add(c.ip)
+		if err != c.expectedErr {
+			t.Errorf("expected err - %+v, actual err - %+v", c.expectedErr, err)
+		}
+	}
+}
+
+func benchmarkAddForIPv4BucketIndex(indexSize int, b *testing.B) {
+	rand.Seed(time.Now().UnixNano())
+
+	index := NewIPv4BucketIndex()
+	for _, ip := range generateIPs(indexSize) {
+		index.Add(ip)
+	}
+
+	ip := generateIP()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		index.Add(ip)
+	}
+}
+
+func BenchmarkAddForIPv4BucketIndex100(b *testing.B)     { benchmarkAddForIPv4BucketIndex(100, b) }
+func BenchmarkAddForIPv4BucketIndex10000(b *testing.B)   { benchmarkAddForIPv4BucketIndex(10000, b) }
+func BenchmarkAddForIPv4BucketIndex100000(b *testing.B)  { benchmarkAddForIPv4BucketIndex(100000, b) }
+func BenchmarkAddForIPv4BucketIndex1000000(b *testing.B) { benchmarkAddForIPv4BucketIndex(1000000, b) }
+
+func randomInt(maxValue int) int {
+	min := 1
+	max := maxValue
+	return rand.Intn(max-min+1) + min
+}
+
+func generateIP() string {
+	return fmt.Sprintf("%d.%d.%d.%d", randomInt(1), randomInt(255), randomInt(255), randomInt(255))
+}
+
+func generateIPs(count int) []string {
+	ips := make([]string, count)
+	for i := range ips {
+		ips[i] = generateIP()
+	}
+	return ips
 }
